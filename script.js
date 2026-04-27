27a97cbc7f6ff9ba761d452a0c22881fdae25225
EDWIN CASTANO GUERRERO <127364027+EdwinCastanoGuerrero@users.noreply.github.com>
2026-04-27 10:01:06 -0300

    chore: exporta cores

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ let blue = document.querySelector("#range_blue");
 
 //cor base
 let colors = [
-    {red: 0, green: 0, blue: 0},
-    {red: 0, green: 0, blue: 0},
-    {red: 0, green: 0, blue: 0},
-    {red: 0, green: 0, blue: 0},
-    {red: 0, green: 0, blue: 0},
+    { red: 0, green: 0, blue: 0 },
+    { red: 50, green: 0, blue: 0 },
+    { red: 100, green: 0, blue: 0 },
+    { red: 150, green: 0, blue: 0 },
+    { red: 255, green: 0, blue: 0 },
 ];
 
 //cor selecionada
@@ -25,13 +25,16 @@ setDefaultColorCards();
 
 setSelectedColorCard(colorCardIndex);
 
-function  setDefaultColorCards(){
+function setDefaultColorCards() {
     colorCards.forEach((card, index) => {
         card.style.backgroundColor = `rgb(${colors[index]?.red || 0},${colors[index]?.green || 0},${colors[index]?.blue || 0})`;
     });
 }
 
-function setSelectedColorCard(index){
+function setSelectedColorCard(index) {
+    
+    // atualizar o índice da cor selecionada
+    colorCardIndex = index;
 
     // remover todas as seleções visuais do HTML
     colorCards.forEach(card => {
@@ -41,10 +44,64 @@ function setSelectedColorCard(index){
     // definir a cor selecionada (visualmente)
     colorCards[index].classList.add('border-3', 'border-white');
 
+
+    //definindo a cor da caixa principal
+    mainColor.style.backgroundColor = `rgb(${colors[index]?.red || 0},${colors[index]?.green || 0},${colors[index]?.blue || 0})`;
+
+    red.value = colors[index]?.red || 0;
+    green.value = colors[index]?.green || 0;
+    blue.value = colors[index]?.blue || 0;
 }
+
+
+red.addEventListener('input', () => {
+    colors[colorCardIndex].red = red.value;
+    updateMainColor();
+    updateColorCard();
+});
+green.addEventListener('input', () => {
+    colors[colorCardIndex].green = green.value;
+    updateMainColor();
+    updateColorCard();
+});
+blue.addEventListener('input', () => {
+    colors[colorCardIndex].blue = blue.value;
+    updateMainColor();
+    updateColorCard();
+});
+
+
+
+//funções
+
+function updateMainColor(){
+    mainColor.style.backgroundColor = `rgb(${colors[colorCardIndex].red},${colors[colorCardIndex].green},${colors[colorCardIndex].blue})`;
+}
+
+function updateColorCard(){
+    colorCards[colorCardIndex].style.backgroundColor = `rgb(${colors[colorCardIndex].red},${colors[colorCardIndex].green},${colors[colorCardIndex].blue})`;
+}
+
+
 
 colorCards.forEach((card, index) => {
     card.addEventListener('click', () => {
         setSelectedColorCard(index);
     });
 });
+
+//exportar as cores
+
+document.querySelector("#btn_export").addEventListener("click", () => {
+    // Lógica para exportar as cores
+    let colorSString = '';
+    colors.forEach(color => {
+        colorSString += `rgb(${color.red}, ${color.green}, ${color.blue})\n`;
+    });
+    navigator.clipboard.writeText(colorSString);
+    document.querySelector("#export_message").classList.remove("hidden");
+    setTimeout(() => {
+        document.querySelector("#export_message").classList.add("hidden");
+    }, 3000);
+});
+
